06c97d9f4613b8e4942ab6fafc9180058bbc810d
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-11 10:29:39 +0200

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,19 +1,7 @@
 import PropTypes from 'prop-types';
-
 import { Section } from 'components/Section/Section';
 
-export function Contacts() {
-  // state = {
-  //  contacts: [
-  // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ],
-  //   filter: '',
-  //   name: '',
-  //   number: '',
-  // };
+export function Contacts({ contacts, deleteContact }) {
   return (
     <Section title="Contacts">
       <label>
@@ -26,10 +14,32 @@ export function Contacts() {
           required
         />
       </label>
-      {/* <ul contacts={this.state.contacts}>
-                <li key={id}><p>{name}:<span>{number}</span></p></li>
-                <button>Delete</button>
-          </ul> */}
+      <ul>
+        {contacts.map(({ id, name, number }) => (
+          <li key={id}>
+            <p>
+              {name}: <span>{number}</span>
+            </p>
+            <button
+              onClick={() => {
+                deleteContact(number);
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </Section>
   );
 }
+
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
