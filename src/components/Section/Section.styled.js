49787944198565d3bb4925665cda05f52d27e23c
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-12 14:42:09 +0200

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.section`
   padding: 40px 40px 30px 40px;
   padding-bottom: 30px;
-  /* background-color: #ffffff; */
 
   &:last-of-type {
     padding-top: 10px;
@@ -15,6 +14,6 @@ export const Title = styled.h2`
   font-weight: 600;
   font-size: 30px;
   letter-spacing: 1.2px;
-  /* color: #545454; */
   text-shadow: 2px 2px 2px #ff6b08;
+  /* text-align: end; */
 `;
